daf68ae8a26b32bf06ebd1097526d2fdae719e08
muhrafifadh <81162174+muhrafifadh@users.noreply.github.com>
2023-05-22 09:25:11 +0700

    AI UPDATE

--- tugas-eas-gamecerdas/Assets/Supercyan Character Pack Free Sample/Scripts/SimpleSampleCharacterControl.cs
+++ tugas-eas-gamecerdas/Assets/Supercyan Character Pack Free Sample/Scripts/SimpleSampleCharacterControl.cs
@@ -6,47 +6,44 @@ public class SimpleSampleCharacterControl : MonoBehaviour
     private enum ControlMode
     {
         /// <summary>
-        /// Up moves the character forward, left and right turn the character gradually and down moves the character backwards
+        /// Character moves in the chosen direction from the perspective of the camera
         /// </summary>
-        Tank,
-        /// <summary>
-        /// Character freely moves in the chosen direction from the perspective of the camera
-        /// </summary>
-        Direct
+        Character
     }
 
-    [SerializeField] private float m_moveSpeed = 2;
-    [SerializeField] private float m_turnSpeed = 200;
-    [SerializeField] private float m_jumpForce = 4;
+    [SerializeField] private float m_moveSpeed = 2f;
+    [SerializeField] private float m_turnSpeed = 200f;
+    [SerializeField] private float m_jumpForce = 4f;
 
     [SerializeField] private Animator m_animator = null;
     [SerializeField] private Rigidbody m_rigidBody = null;
 
-    [SerializeField] private ControlMode m_controlMode = ControlMode.Direct;
+    [SerializeField] private ControlMode m_controlMode = ControlMode.Character;
 
-    private float m_currentV = 0;
-    private float m_currentH = 0;
+    private float m_currentV = 0f;
+    private float m_currentH = 0f;
 
-    private readonly float m_interpolation = 10;
+    private readonly float m_interpolation = 10f;
     private readonly float m_walkScale = 0.33f;
     private readonly float m_backwardsWalkScale = 0.16f;
     private readonly float m_backwardRunScale = 0.66f;
 
     private bool m_wasGrounded;
-    private Vector3 m_currentDirection = Vector3.zero;
-
-    private float m_jumpTimeStamp = 0;
-    private float m_minJumpInterval = 0.25f;
     private bool m_jumpInput = false;
 
     private bool m_isGrounded;
-
     private List<Collider> m_collisions = new List<Collider>();
+
+    private float m_jumpTimeStamp = 0f;
+    private readonly float m_minJumpInterval = 0.25f;
 
     private void Awake()
     {
-        if (!m_animator) { gameObject.GetComponent<Animator>(); }
-        if (!m_rigidBody) { gameObject.GetComponent<Animator>(); }
+        if (!m_animator)
+            m_animator = GetComponent<Animator>();
+
+        if (!m_rigidBody)
+            m_rigidBody = GetComponent<Rigidbody>();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -73,7 +70,8 @@ public class SimpleSampleCharacterControl : MonoBehaviour
         {
             if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
             {
-                validSurfaceNormal = true; break;
+                validSurfaceNormal = true;
+                break;
             }
         }
 
@@ -91,7 +89,10 @@ public class SimpleSampleCharacterControl : MonoBehaviour
             {
                 m_collisions.Remove(collision.collider);
             }
-            if (m_collisions.Count == 0) { m_isGrounded = false; }
+            if (m_collisions.Count == 0)
+            {
+                m_isGrounded = false;
+            }
         }
     }
 
@@ -101,7 +102,10 @@ public class SimpleSampleCharacterControl : MonoBehaviour
         {
             m_collisions.Remove(collision.collider);
         }
-        if (m_collisions.Count == 0) { m_isGrounded = false; }
+        if (m_collisions.Count == 0)
+        {
+            m_isGrounded = false;
+        }
     }
 
     private void Update()
@@ -118,12 +122,8 @@ public class SimpleSampleCharacterControl : MonoBehaviour
 
         switch (m_controlMode)
         {
-            case ControlMode.Direct:
-                DirectUpdate();
-                break;
-
-            case ControlMode.Tank:
-                TankUpdate();
+            case ControlMode.Character:
+                CharacterUpdate();
                 break;
 
             default:
@@ -135,7 +135,7 @@ public class SimpleSampleCharacterControl : MonoBehaviour
         m_jumpInput = false;
     }
 
-    private void TankUpdate()
+    private void CharacterUpdate()
     {
         float v = Input.GetAxis("Vertical");
         float h = Input.GetAxis("Horizontal");
@@ -144,8 +144,14 @@ public class SimpleSampleCharacterControl : MonoBehaviour
 
         if (v < 0)
         {
-            if (walk) { v *= m_backwardsWalkScale; }
-            else { v *= m_backwardRunScale; }
+            if (walk)
+            {
+                v *= m_backwardsWalkScale;
+            }
+            else
+            {
+                v *= m_backwardRunScale;
+            }
         }
         else if (walk)
         {
@@ -155,45 +161,17 @@ public class SimpleSampleCharacterControl : MonoBehaviour
         m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
         m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
 
-        transform.position += transform.forward * m_currentV * m_moveSpeed * Time.deltaTime;
-        transform.Rotate(0, m_currentH * m_turnSpeed * Time.deltaTime, 0);
+        Vector3 moveDirection = transform.forward * m_currentV + transform.right * m_currentH;
+        moveDirection.y = 0f;
+        moveDirection.Normalize();
 
-        m_animator.SetFloat("MoveSpeed", m_currentV);
+        transform.position += moveDirection * m_moveSpeed * Time.deltaTime;
 
-        JumpingAndLanding();
-    }
+        // Apply rotation based on mouse movement
+        float mouseX = Input.GetAxis("Mouse X") * m_turnSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.up, mouseX);
 
-    private void DirectUpdate()
-    {
-        float v = Input.GetAxis("Vertical");
-        float h = Input.GetAxis("Horizontal");
-
-        Transform camera = Camera.main.transform;
-
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            v *= m_walkScale;
-            h *= m_walkScale;
-        }
-
-        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
-        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
-
-        Vector3 direction = camera.forward * m_currentV + camera.right * m_currentH;
-
-        float directionLength = direction.magnitude;
-        direction.y = 0;
-        direction = direction.normalized * directionLength;
-
-        if (direction != Vector3.zero)
-        {
-            m_currentDirection = Vector3.Slerp(m_currentDirection, direction, Time.deltaTime * m_interpolation);
-
-            transform.rotation = Quaternion.LookRotation(m_currentDirection);
-            transform.position += m_currentDirection * m_moveSpeed * Time.deltaTime;
-
-            m_animator.SetFloat("MoveSpeed", direction.magnitude);
-        }
+        m_animator.SetFloat("MoveSpeed", moveDirection.magnitude);
 
         JumpingAndLanding();
     }
